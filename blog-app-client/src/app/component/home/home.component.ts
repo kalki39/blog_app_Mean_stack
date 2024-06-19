2fa1f50c39153cfa0752ff2380e1ae747ac991ab
kalki39 <kalkiram39@gmail.com>
2024-06-19 20:41:28 +0530

    auth_updated

--- blog-app-client/src/app/component/home/home.component.ts
+++ blog-app-client/src/app/component/home/home.component.ts
@@ -8,45 +8,48 @@ import { StorageService } from 'src/app/service/storage.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   blogs: any[] = [];
   followersList: any[] = [];
   followingList: any[] = [];
   updatedList: any[] = [];
   currentUser: any = {};
-  isEdit:boolean=false;
-  editBoldData:any={};
-  activeBtn=1;
-  loading=false;
+  isEdit: boolean = false;
+  editBoldData: any = {};
+  activeBtn = 1;
+  loading = false;
 
   constructor(
     private apicallService: ApicallService,
     public authService: AuthGuardService,
-    private router: Router,private snackbarService:SnackbarService,
-    private st:StorageService
-  ) { }
+    private router: Router,
+    private snackbarService: SnackbarService,
+    private st: StorageService
+  ) {}
 
   ngOnInit() {
-    this.loading=true;
-  this.currentUser=this.getCurrentUser();
-  this.getBlogs();
+    this.loading = true;
+    if (!this.getCurrentUser()) {
+      this.router.navigate(['/login']);
+    }
+    this.currentUser = this.getCurrentUser();
+    this.getBlogs();
   }
 
   getCurrentUser() {
     // Fetch the current user (mocked data for this example)
-    return this.st.getStorage('user') // Replace with actual user fetching logic
+    return this.st.getStorage('user'); // Replace with actual user fetching logic
   }
 
   getBlogs() {
-    this.activeBtn=1;
-    this.loading=true;
-    this.blogs=[]
+    this.activeBtn = 1;
+    this.loading = true;
+    this.blogs = [];
     this.apicallService.getBlogs(this.currentUser.userId).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         this.blogs = response.date; // Adjust based on your API response structure
       },
       (error: any) => {
@@ -55,11 +58,11 @@ export class HomeComponent implements OnInit {
     );
   }
   getAllBlog() {
-    this.activeBtn=0;
-    this.loading=true;
+    this.activeBtn = 0;
+    this.loading = true;
     this.apicallService.getAllBlogs(this.currentUser.userId).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         this.blogs = response.date; // Adjust based on your API response structure
       },
       (error: any) => {
@@ -68,11 +71,11 @@ export class HomeComponent implements OnInit {
     );
   }
   getFollowersList() {
-    this.activeBtn=2;
-    this.loading=true;
+    this.activeBtn = 2;
+    this.loading = true;
     this.apicallService.getFollowersList(this.currentUser.userId).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         this.followersList = response.data; // Adjust based on your API response structure
         this.getFollowingList(false);
       },
@@ -82,32 +85,32 @@ export class HomeComponent implements OnInit {
     );
   }
   findPeople() {
-    this.activeBtn=4;
-    this.loading=true;
+    this.activeBtn = 4;
+    this.loading = true;
     this.apicallService.findPeople(this.currentUser.userId).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         console.log(response);
-        this.updatedList = response.data; 
+        this.updatedList = response.data;
       },
       (error: any) => {
         console.error('Failed to fetch blogs', error);
       }
     );
   }
-  unfollow(data:any){
-    this.loading=true;
-    data={followingUserId:data._id}
-    this.apicallService.unfollowUser(this.currentUser.userId,data).subscribe(
+  unfollow(data: any) {
+    this.loading = true;
+    data = { followingUserId: data._id };
+    this.apicallService.unfollowUser(this.currentUser.userId, data).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         console.log(response);
-        if(response.status==200){
+        if (response.status == 200) {
           this.snackbarService.show(response.message);
-          if(this.activeBtn==3)this.getFollowingList(true);
-          if(this.activeBtn==2)this.getFollowersList();
+          if (this.activeBtn == 3) this.getFollowingList(true);
+          if (this.activeBtn == 2) this.getFollowersList();
         }
-        this.updatedList = response.data; 
+        this.updatedList = response.data;
       },
       (error: any) => {
         console.error('Failed to fetch blogs', error);
@@ -115,40 +118,43 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  follow(data:any){
-    this.loading=true;
-    data={followingUserId:data._id}
-    this.apicallService.followUser(this.currentUser.userId,data).subscribe(
+  follow(data: any) {
+    this.loading = true;
+    data = { followingUserId: data._id };
+    this.apicallService.followUser(this.currentUser.userId, data).subscribe(
       (response: any) => {
         console.log(response);
-        this.loading=false;
-        if(response.status==200){
+        this.loading = false;
+        if (response.status == 200) {
           this.snackbarService.show(response.message);
-          if(this.activeBtn==3)this.getFollowingList(true);
-          if(this.activeBtn==2)this.getFollowersList();
-          if(this.activeBtn==4)this.findPeople();
+          if (this.activeBtn == 3) this.getFollowingList(true);
+          if (this.activeBtn == 2) this.getFollowersList();
+          if (this.activeBtn == 4) this.findPeople();
         }
-        this.updatedList = response.data; 
+        this.updatedList = response.data;
       },
       (error: any) => {
         console.error('Failed to fetch blogs', error);
       }
     );
   }
-  getFollowingList(fromButton:boolean) {
-    this.loading=true;
+  getFollowingList(fromButton: boolean) {
+    this.loading = true;
     console.log(fromButton);
-    if(fromButton){
-      this.activeBtn=3;
+    if (fromButton) {
+      this.activeBtn = 3;
     }
     this.apicallService.getFollowingList(this.currentUser.userId).subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         console.log(response);
-        if(fromButton){
-          this.updatedList = response.data; 
-        }else{
-          this.updatedList=this.checkUpdateList(this.followersList,this.updatedList)
+        if (fromButton) {
+          this.updatedList = response.data;
+        } else {
+          this.updatedList = this.checkUpdateList(
+            this.followersList,
+            this.updatedList
+          );
         }
       },
       (error: any) => {
@@ -157,31 +163,31 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  checkUpdateList(followerarr:any,followingarr:any){
-    let arr: any[]=[]
-    followerarr.forEach((follower:any) => {
-        if(followingarr.some((item:any)=>item._id==follower._id)){
-          follower.following=true;
-        }else{
-          follower.following=false;
-        }
-        arr.push(follower)
+  checkUpdateList(followerarr: any, followingarr: any) {
+    let arr: any[] = [];
+    followerarr.forEach((follower: any) => {
+      if (followingarr.some((item: any) => item._id == follower._id)) {
+        follower.following = true;
+      } else {
+        follower.following = false;
+      }
+      arr.push(follower);
     });
-    console.log("up",arr);
+    console.log('up', arr);
     return arr;
   }
 
   logout() {
     // Perform logout logic (clear tokens, etc.)
-    this.loading=true;
+    this.loading = true;
     this.apicallService.logout().subscribe(
       (response: any) => {
-        this.loading=false;
+        this.loading = false;
         this.snackbarService.show(response.message);
-        this.st.removeStorage('user')
-        if(response.status==200){
+        this.st.removeStorage('user');
+        if (response.status == 200) {
           this.router.navigate(['/login']);
-        } 
+        }
       },
       (error: any) => {
         console.error('Failed to logout', error);
@@ -189,44 +195,42 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  edit(blog:any){
+  edit(blog: any) {
     console.log(blog);
-    if(this.checkTimeExceed30min(blog)){
+    if (this.checkTimeExceed30min(blog)) {
       return;
     }
-    this.editBoldData=blog;
-    this.isEdit=true
+    this.editBoldData = blog;
+    this.isEdit = true;
   }
-  checkTimeExceed30min(blog:any){
+  checkTimeExceed30min(blog: any) {
     // let createTime=new Date(blog.creationDateTime).getTime()
     const creationDateTime = new Date(blog.creationDateTime).getTime();
     const currentDateTime = Date.now();
     const diff = (currentDateTime - creationDateTime) / (1000 * 60);
     console.log(creationDateTime);
     console.log(currentDateTime);
-    if(diff>30){
-      this.snackbarService.show('can not edit this blog, 30mins exceeds')
+    if (diff > 30) {
+      this.snackbarService.show('can not edit this blog, 30mins exceeds');
       return true;
     }
     return false;
   }
-  deleteBlog(blog:any){
-    this.loading=true;
-    this.apicallService.deleteBlogs(blog).subscribe((res:any)=>{
+  deleteBlog(blog: any) {
+    this.loading = true;
+    this.apicallService.deleteBlogs(blog).subscribe((res: any) => {
       this.snackbarService.show(res.message);
-      this.loading=false;
-      if(res.status==200){
+      this.loading = false;
+      if (res.status == 200) {
         this.getBlogs();
       }
-    })
+    });
   }
 
-  changeState(val:any){
-    this.isEdit=val;
+  changeState(val: any) {
+    this.isEdit = val;
     this.getBlogs();
   }
 
-  navigateTo(blog:any){
-
-  }
+  navigateTo(blog: any) {}
 }
--- blog-app-client/src/app/component/login/login.component.ts
+++ blog-app-client/src/app/component/login/login.component.ts
@@ -7,65 +7,82 @@ import { StorageService } from 'src/app/service/storage.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  loginForm! : FormGroup;
+  loginForm!: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private apicallService: ApicallService,
-    private snackbarService:SnackbarService,
-    private storageService:StorageService,
-    private router: Router,
-  ) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apicallService: ApicallService,
+    private snackbarService: SnackbarService,
+    private storageService: StorageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    if (this.getCurrentUser()) {
+      this.router.navigate(['/home']);
+    }
     this.loginForm = this.formBuilder.group({
       loginId: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  get f() { return this.loginForm?.controls; }
+  get f() {
+    return this.loginForm?.controls;
+  }
+  getCurrentUser() {
+    // Fetch the current user (mocked data for this example)
+    return this.storageService.getStorage('user'); // Replace with actual user fetching logic
+  }
 
   onSubmit() {
-    this.submitted= true;
+    this.submitted = true;
 
-    if(!this.loginForm.valid){
-     return;
+    if (!this.loginForm.valid) {
+      return;
     }
 
-    let data=this.loginForm.value;
+    let data = this.loginForm.value;
     this.apicallService.login(data).subscribe(
-      loginResponse => {
+      (loginResponse) => {
         this.snackbarService.show(loginResponse.message);
         console.log(loginResponse);
-        
-        if(loginResponse.status==200){
-          if(loginResponse.data){
-            let {name,email,username,_id,img} = loginResponse.data;
-            this.storageService.setStorage('user',{name,email,username,userId:_id,img})
+
+        if (loginResponse.status == 200) {
+          if (loginResponse.data) {
+            let { name, email, username, _id, img } = loginResponse.data;
+            this.storageService.setStorage('user', {
+              name,
+              email,
+              username,
+              userId: _id,
+              img,
+            });
             console.log(this.storageService.getStorage('user'));
-           }
+          }
 
           this.router.navigate(['/home']);
         }
       },
-      loginError => {
+      (loginError) => {
         console.log(loginError);
         this.snackbarService.show(loginError.error.message);
       }
     );
-    this.apicallService.login(data).subscribe((res:any)=>{
-      console.log(res)
-      this.snackbarService.show(res?.message);
-      this.loginForm.reset(); // Reset form values after successful login
-      this.submitted = false;
-    },
-    (error:any)=>{
-      this.snackbarService.show('registration failed');
-    })
+    this.apicallService.login(data).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.snackbarService.show(res?.message);
+        this.loginForm.reset(); // Reset form values after successful login
+        this.submitted = false;
+      },
+      (error: any) => {
+        this.snackbarService.show('registration failed');
+      }
+    );
   }
-
 }
